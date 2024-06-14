44759560dc4e64e298eff13644304073267cbfc0
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-06-14 10:08:13 -0400

    adds countdown functionality, alternating between three different countdowns depending on the phase the user is in

--- main.py
+++ main.py
@@ -6,23 +6,33 @@ root = Tk()
 root.title("Pomodoro Timer")
 root.minsize(1000, 1000)
 
-# converts the minutes into seconds
-work_time = 1 * 60  
-short_break = 2 * 60  
-long_break = 3 * 60  
-count = work_time  # Initial countdown value
-
 
 # imitates the countdown and updates the display using recursion
+session = 1
 
-def update_time():
-    global count
-    if count > 0:
+def determine_session():
+    global session
+  
+    work_time = 5  # Using smaller values for testing
+    short_break = 2 * 60  # 2 minutes
+    long_break = 3 * 60  # 3 minutes
+    
+    if session % 8 == 0:
+        update_time(long_break)
+    elif session % 2 == 0:
+        update_time(short_break)
+    else:
+        update_time(work_time)
+    
+    session += 1
+ 
+def update_time(count):
+    if count >= 0:
         mins, secs = divmod(count, 60)
         time_format = f'{mins:02}:{secs:02}'
         canvas.itemconfig(timer_text, text=time_format)
         count -= 1
-        root.after(1000, update_time)
+        root.after(1000, update_time, count)
     else:
         pass
 
@@ -38,7 +48,7 @@ timer_text = canvas.create_text(500, 500, text="25:00", fill="white", font=("Ari
 canvas.pack()
 
 # displays the button to start countdown
-button = Button(root, text="Start Timer", font=("Arial", 16, "bold"), bg="white", height=3, width=30, command=update_time)
+button = Button(root, text="Start Timer", font=("Arial", 16, "bold"), bg="white", height=3, width=30, command=determine_session)
 button.place(relx=0.5, rely=0.7, anchor="center")
 
 root.mainloop()
